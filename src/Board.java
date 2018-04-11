046de4940a15b79abb4f75e5c568aa6b95518262
Bastien Duret <bastien.duret@gmail.com>
2018-04-11 16:49:58 +0200

    Add isGoal and equals

--- src/Board.java
+++ src/Board.java
@@ -19,11 +19,33 @@ public class Board {
         return size;
     }
 
-//    public int hamming()                   // number of blocks out of place
+    //    public int hamming()                   // number of blocks out of place
 //    public int manhattan()                 // sum of Manhattan distances between blocks and goal
-//    public boolean isGoal()                // is this board the goal board?
+
+    public boolean isGoal() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == size - 1 && j == size - 1) {
+                    return blocks[i][j] == 0;
+                } else if (blocks[i][j] != 1 + size * i + j) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 //    public Board twin()                    // a board that is obtained by exchanging any pair of blocks
-//    public boolean equals(Object y)        // does this board equal y?
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (other == null) return false;
+        if (other.getClass() != this.getClass()) return false;
+        Board that = (Board) other;
+        return that.toString() == this.toString();
+    }
+
 //    public Iterable<Board> neighbors()     // all neighboring boards
 
     public String toString() {
--- test/BoardTest.java
+++ test/BoardTest.java
@@ -4,10 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
+    Board makeBoard(int size) {
+        return new Board(new int[size][size]);
+    }
+
     @Test
     void output() {
-        int[][] blocks = new int[3][3];
-        Board board = new Board(blocks);
+        Board board = makeBoard(3);
         assertEquals("3\n" +
                 " 0  0  0 \n" +
                 " 0  0  0 \n" +
@@ -16,9 +19,46 @@ class BoardTest {
 
     @Test
     void dimension() {
-        int[][] blocks = new int[3][3];
-        Board board = new Board(blocks);
+        Board board = makeBoard(3);
         assertEquals(3, board.dimension());
+    }
+
+
+    @Test
+    void isGoal__false() {
+        Board board = makeBoard(3);
+        assertFalse(board.isGoal());
+    }
+
+    @Test
+    void isGoal__true() {
+        int[][] blocks = new int[3][3];
+        blocks[0][0] = 1;
+        blocks[0][1] = 2;
+        blocks[0][2] = 3;
+        blocks[1][0] = 4;
+        blocks[1][1] = 5;
+        blocks[1][2] = 6;
+        blocks[2][0] = 7;
+        blocks[2][1] = 8;
+        Board board = new Board(blocks);
+        assertTrue(board.isGoal());
+    }
+
+    @Test
+    void equals__false() {
+        Board board = makeBoard(3);
+        assertFalse(board.equals(null));
+        assertFalse(board.equals(makeBoard(2)));
+        int[][] otherBlocks = new int[3][3];
+        otherBlocks[0][0] = 3;
+        assertFalse(board.equals(new Board(otherBlocks)));
+    }
+
+    @Test
+    void equals__true() {
+        Board board = makeBoard(3);
+        assertFalse(board.equals(makeBoard(3)));
     }
 
 
